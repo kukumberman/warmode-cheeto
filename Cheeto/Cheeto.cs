5dcfc396e7bc4b05ec5c0db54d1959f5bef81c78
Dima <strongbaby1337@gmail.com>
2022-03-12 01:09:18 +0200

    cheeto loader

---
+++ Cheeto/Cheeto.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using BepInEx.Logging;
+
+namespace Cheeto
+{
+    class Cheeto : MonoBehaviour
+    {
+        private static string ApplicationInfo = "";
+        private static ManualLogSource Logger = null;
+
+        public static void Run(Plugin loader)
+        {
+            Logger = loader.Log;
+
+            ApplicationInfo = string.Format("{0} {1} {2} {3}", Application.productName, Application.version, Application.unityVersion, Application.buildGUID);
+            Logger.LogInfo(ApplicationInfo);
+
+            Cheeto instance = loader.AddComponent<Cheeto>();
+            DontDestroyOnLoad(instance.gameObject);
+            instance.hideFlags |= HideFlags.HideAndDontSave;
+
+            CodeStage.AntiCheat.Detectors.ObscuredCheatingDetector.StopDetection();
+            CodeStage.AntiCheat.Detectors.ObscuredCheatingDetector.Dispose();
+            CodeStage.AntiCheat.Detectors.SpeedHackDetector.StopDetection();
+            CodeStage.AntiCheat.Detectors.SpeedHackDetector.Dispose();
+            CodeStage.AntiCheat.Detectors.InjectionDetector.StopDetection();
+            CodeStage.AntiCheat.Detectors.InjectionDetector.Dispose();
+        }
+    }
+}
--- Cheeto/Plugin.cs
+++ Cheeto/Plugin.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BepInEx;
 
 namespace Cheeto
 {
-    public class Plugin
+    [BepInPlugin("warmode-cheeto", "cucumba", "1.4.6")]
+    public class Plugin : BepInEx.IL2CPP.BasePlugin
     {
+        public override void Load()
+        {
+            Log.LogInfo("cucumba loaded");
+            Cheeto.Run(this);
+        }
     }
 }
